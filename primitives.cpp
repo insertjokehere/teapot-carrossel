37f764cafd70a8058de4ff8bcba6a3a29effb895
William Hughes <whlabratz@gmail.com>
2013-03-24 11:48:44 +1300

    fixed one side of cube not being drawn

--- primitives.cpp
+++ primitives.cpp
@@ -10,12 +10,12 @@ cube::cube(float width, float height, float depth, const float* colour, transfor
 void cube::draw() {
 	debug("cube::draw()");
 
-	float body[][3]  = { {0,0,0},{0,height,0}, {0,0,depth},{0,height,depth}, {width,0,depth},{width,height,depth},{width,0,0},{width,height,0}};
+	float body[][3]  = { {0,0,0},{0,height,0}, {0,0,depth},{0,height,depth}, {width,0,depth},{width,height,depth},{width,0,0},{width,height,0},{0,0,0},{0,height,0}};
 	float top[][3] = { {0,height,0},{0,height,depth},{width,height,depth},{width,height,0} };
 	float bottom[][3] = { {0,0,0},{0,0,depth},{width,0,depth},{width,0,0} };
 
 	colorV(colour);
-	object::drawVertex(body, 8, GL_QUAD_STRIP);
+	object::drawVertex(body, 10, GL_QUAD_STRIP);
 	object::drawVertex(top,4,GL_POLYGON);
 	object::drawVertex(bottom,4,GL_POLYGON);
 }
